8f00eec217aff7cb9f7b0214f2818f1350c4eb9b
Saidazimbek-25 <ilyosovsaidazimbek23@gmail.com>
2025-05-22 11:20:15 +0500

    Update lesson1.py

--- lesson-1/homework/lesson1.py
+++ lesson-1/homework/lesson1.py
@@ -1,5 +1,28 @@
+# 1 task
 side=float(input('enter the length of the side: '))
 parimeter=4*side
 area=side**2
 print('Parimeter:',parimeter)
 print('Area:',area)
+# 2 task 
+diameter = float(input('Enter the diameter:'))
+pi = 3.14
+length = pi * diameter
+print('Length of the circle:', length)
+# 3 task
+a=float(input('Enter the number'))
+b=float(input('Enter the number'))
+Mean=(a+b)/2
+print('Average is : ', Mean)
+# 5 task 
+a=float(input('Enter the number'))
+b=float(input('Enter the number'))
+sum=(a+b)
+product=(a*b)
+square_a=a**2
+square_b=b**2
+
+print('SUM:',sum)
+print('Product:',product)
+print('Square of a:',square_a)
+print('Square of b:',square_b)
