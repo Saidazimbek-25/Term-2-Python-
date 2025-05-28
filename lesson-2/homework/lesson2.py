0340348e5d212960251e24c067ad87b8e3944d8a
Saidazimbek-25 <ilyosovsaidazimbek23@gmail.com>
2025-05-28 15:52:13 +0500

    Update lesson2.py

--- lesson-2/homework/lesson2.py
+++ lesson-2/homework/lesson2.py
@@ -12,3 +12,18 @@ name=input('Enter your name:')
 
 birthdate = date(year, month, day)
 print(f"Hello  {name}! You are {age_calculator(birthdate)} years old.")
+#task-2
+txt = 'LMaasleitbtui'
+print(txt[::2]) #output is Lasetti
+print(txt[1::2]) #output is Malibu
+#task-3
+txt = 'MsaatmiazD'
+print(txt[::2]) #output is Matiz
+print(txt[-1::-2]) #output is Damas
+#task-3
+txt = "I'am John. I am from London" 
+print(txt[-6:]) #output is London
+#task-4
+a = input('Enter the word you want, I will make it reverse: ')
+reversed = a[::-1]
+print(f'Result: {reversed}')
