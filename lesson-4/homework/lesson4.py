724e40a99a013f6e42cdd28fec8a49fca67b14e3
Saidazimbek-25 <ilyosovsaidazimbek23@gmail.com>
2025-06-05 13:19:44 +0500

    Update lesson4.py

--- lesson-4/homework/lesson4.py
+++ lesson-4/homework/lesson4.py
@@ -13,9 +13,13 @@ print(a)
 a.update({2:30})
 print(a)
 #task-3
+#1st way
 dic1 = {1: 10, 2: 20}
 dic2 = {3: 30, 4: 40}
 dic3 = {5: 50, 6: 60}
 dic1.update(dic2)
 dic1.update(dic3)
+print(dic1)
+#second way
+dic1=dic1|dic2|dic3
 print(dic1)
