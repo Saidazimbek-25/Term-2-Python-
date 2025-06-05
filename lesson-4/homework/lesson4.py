86cb1319f1942130cf9c7e32ad96b814f57c14ed
Saidazimbek-25 <ilyosovsaidazimbek23@gmail.com>
2025-06-05 13:01:27 +0500

    Update lesson4.py

--- lesson-4/homework/lesson4.py
+++ lesson-4/homework/lesson4.py
@@ -1,1 +1,6 @@
-
+#task-1
+my_dict={"apple":2,"banana":30,"kiwi":12,"pear":90}
+asc_sorted=dict(sorted(my_dict.items(),key=lambda item:item[1]))
+desc_sorted = dict(sorted(my_dict.items(), key=lambda item: item[1],reverse=True))
+print("Acsending order",asc_sorted)
+print("Decsending order",desc_sorted)
