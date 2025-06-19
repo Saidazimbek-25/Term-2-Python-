4d25f41687a91720e7d97e3de9b9dc16ee61c701
SAID@ZIMBEK <ilyosovsaidazimbek23@gmail.com>
2025-06-19 19:37:36 +0500

    Update lesson17.py

--- lesson-17/homework/lesson17.py
+++ lesson-17/homework/lesson17.py
@@ -75,3 +75,20 @@ fourthway=df.filter(regex='First Name|City')
 
 print(fourthway)
 
+#Task-5
+import pandas as pd
+
+import numpy as np
+
+df = pd.DataFrame({
+    'First Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    
+    'Age': [25, 30, 35, 40],
+    'City': ['New York', 'San Francisco', 'Los Angeles', 'Chicago']
+    
+})
+
+
+df['Salary']=np.random.randint(3000,10000,size=len(df))
+print(df)
+
