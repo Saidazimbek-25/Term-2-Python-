16d98f988053338a9caf4a154daeca4e3645ef52
SAID@ZIMBEK <ilyosovsaidazimbek23@gmail.com>
2025-06-23 10:27:35 +0500

    Update dicts.py

--- lesson-4/homework/dicts.py
+++ lesson-4/homework/dicts.py
@@ -26,3 +26,37 @@ print(dic1)
 #3rd way
 d3 = {**dic1, **dic2,**dic3}
 print(d3)
+#4. Generate a Dictionary with Squares
+n = int(input('Enter a number: '))
+i = 1
+dic = {}
+while i <= n:
+    dic[i] = i*i
+    i += 1
+print(dic)
+#5. Dictionary of Squares (1 to 15)
+n = 15
+i = 1
+dic = {}
+while i <= n:
+    dic[i] = i*i
+    i += 1
+print(dic)
+#1. Create a Set
+st = {1, 4, 3, 15, 80, 3, 38, 2, 6, 8, 32, 5, 35}
+print(st)
+#2. Iterate Over a Set
+st = {1, 4, 3, 15, 80, 3, 38, 2, 6, 8, 32, 5, 35}
+for x in st:
+    print(x)
+#Add Member(s) to a Set
+st = {1, 4, 3, 15, 80, 3, 38, 2, 6, 8, 32, 5, 35}
+st.update([100, 0, 123,50])
+print(st)
+#4. Remove Item(s) from a Set
+st = {1, 4, 3, 15, 80, 3, 38, 2, 6, 8, 32, 5, 35}
+st.remove(999)
+print(st)
+#5. Remove an Item if Present in the Set
+st.discard(100)
+print(st)
