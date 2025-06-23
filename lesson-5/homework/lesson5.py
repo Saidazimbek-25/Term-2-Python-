7e7309b559b305c92e5791af167495f0333091cc
SAID@ZIMBEK <ilyosovsaidazimbek23@gmail.com>
2025-06-23 10:29:00 +0500

    Create lesson5.py

---
+++ lesson-5/homework/lesson5.py
@@ -0,0 +1,32 @@
+
+#1.  Determine whether a given year is a leap year.
+def is_leap(year):
+    if not isinstance(year, int):
+        raise ValueError("Year must be an integer.")
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+#2. Conditional Statements Exercise
+n = int(input('Input an integer bewtween 1 and 100: '))
+while not 1 <= n <= 100:
+    n = int(input('Input an integer bewtween 1 and 100: '))
+if (n % 2 != 0) or (n % 2 == 0 and 6 <= n <= 20):
+    print('Weird')
+else:
+    print('Not weird')
+#3. Given two integer numbers a and b. Find even numbers between this numbers. a and b are inclusive. Don't use loop.
+#Solution 1 with if-else statement.
+a = int(input('Enter a starting position: '))
+b = int(input('Enter an ending position: '))
+if a % 2 == 0:
+    numbers = list(range(a, b + 1, 2))
+else:
+    numbers = list(range(a + 1 , b + 1, 2))
+print(numbers)
+#Solution 2 without if-else statement.
+a = int(input('Enter a starting position: '))
+b = int(input('Enter an ending position: '))
+numbers = list(range(a, b + 1))
+print([x for x in numbers if x % 2 == 0])
